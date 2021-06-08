58856904d05544d0e2312a972191384963f12c10
Babak Moazzez <babakmoazzez@gmail.com>
2021-06-08 14:41:01 -0400

    Add files via upload

--- SVM_vs_NN.py
+++ SVM_vs_NN.py
@@ -1,8 +1,12 @@
+from tensorflow.python.keras.layers import Dense,Conv2D,Flatten,MaxPooling2D,ZeroPadding2D,Dropout,Softmax
 from sklearn.model_selection import train_test_split,GridSearchCV
 from sklearn.metrics import classification_report
+from tensorflow.keras.utils import to_categorical
 from sklearn.neural_network import MLPClassifier
+from tensorflow.python.keras import Sequential
 from sklearn.datasets import fetch_lfw_people
 from sklearn.svm import SVC
+import tensorflow as tf
 import numpy as np
 import time
 
@@ -26,6 +30,7 @@ m = X.shape[0] #number of training examples
 K = lib['target_names'].shape[0] #number of classes
 
 X_train,X_test,y_train,y_test = train_test_split(X,y,train_size = 0.8,test_size = 0.2, shuffle = True)
+
 
 #Classification using Support Vector Machine 
 #Use 5-fold cross validation to find the best regularization parameter and kernel
@@ -69,5 +74,54 @@ print("best NN is with ", max(score,key=score.get))
 y_predicted = NN[max(score)].predict(X_test)
 print(classification_report(y_test,y_predicted,target_names=names))
 
+#========================================================================================#
+   
+#Classification using Convolutional Neural Networks in Tensorflow/Keras
+model=Sequential()
+model.add(Conv2D(64,kernel_size=3,activation='relu',input_shape=(width,height,1)))
+model.add(MaxPooling2D((2,2),strides=(2,2)))
+model.add(Conv2D(128,kernel_size=3,activation='relu'))
+model.add(MaxPooling2D((2,2),strides=(2,2)))
+model.add(Conv2D(256,kernel_size=5,activation='relu'))
+model.add(MaxPooling2D((2,2),strides=(2,2)))
+#model.add(Dropout(0.25))
+model.add(Conv2D(512,kernel_size=3,activation='relu'))
+model.add(MaxPooling2D((2,2),strides=(2,2)))
+model.add(Flatten())
+model.add(Dense(7680,activation='relu'))
+model.add(Dense(3840,activation='relu'))
+model.add(Dense(960,activation='relu'))
+model.add(Dense(K,activation='softmax'))
 
-#CNNs to be added here soon
+#reshape to prepare input
+X_train=X_train.reshape(X_train.shape[0],width,height,1)
+X_test=X_test.reshape(X_test.shape[0],width,height,1)
+
+y_train=to_categorical(y_train)
+y_test=to_categorical(y_test)
+
+
+model.compile(optimizer='Adamax',loss='categorical_crossentropy',metrics=['accuracy'])
+model.fit(X_train,y_train,validation_data=(X_test,y_test),batch_size=100,epochs=25)
+
+score=model.evaluate(X_test,y_test,verbose=0)
+print(score[1])
+
+y_predicted = model.predict(X_test)
+
+y_test = [np.argmax(i) for i in y_predicted]
+y_test = [np.argmax(i) for i in y_test]
+
+print(classification_report(y_test,y_predicted,target_names=names))
+
+
+
+
+
+
+
+
+
+
+
+    
